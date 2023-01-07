d252595a4fc4023fbde212e03ce44aae0e8b59fb
Abheeta A <abheeta09@gmail.com>
2023-01-07 18:37:36 +0530

    update dto

--- src/student/dto/update-student.dto.ts
+++ src/student/dto/update-student.dto.ts
@@ -1,10 +1,34 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateStudentDto } from './create-student.dto';
+import { Type } from 'class-transformer';
+import { IsArray, IsIn, IsNotEmpty, IsString, ValidateBy, ValidateIf, ValidateNested } from 'class-validator';
+
+export class UpdateStudentDto {
+    @IsArray()
+    @ValidateNested({each: true})
+    @Type(() => UpdateDescDto)
+    readonly updateOps: UpdateDescDto[];
+}
+
+export class UpdateDescDto {
+    @ValidateIf(
+        (updateOp: UpdateDescDto) => !(
+            (updateOp.op === "replace" && updateOp.path !== "courses") || 
+            ((updateOp.op === "add" || updateOp.op === "remove") && updateOp.path === "courses")
+        ) 
+    )
+    @IsNotEmpty()
+    @IsString()
+    @IsIn([""], {
+        message: "op must be one of the following values: add, remove, replace. Also, add & remove can only be paired with path=courses, and replace can only be paired with path=name or path=class"
+    })
+    readonly op: string;
 
 
-export class UpdateStudentDto extends PartialType(CreateStudentDto){
+    @IsNotEmpty()
+    @IsString()
+    @IsIn(["name", "class", "courses"])
+    readonly path: string;
 
     
-
-
+    @IsNotEmpty()
+    readonly value;
 }
